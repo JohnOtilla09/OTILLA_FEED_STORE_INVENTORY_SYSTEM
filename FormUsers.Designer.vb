0167b1dea2556cde2c09b3ecdc7efc0ba14ff88f
John Otilla <johncanila.otilla@bicol-u.edu.ph>
2023-04-17 22:25:22 +0800

    Add project files.

---
+++ FormUsers.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormUsers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.addBtn = New System.Windows.Forms.Button()
+        Me.dgUserTable = New System.Windows.Forms.DataGridView()
+        Me.searchButton = New FontAwesome.Sharp.IconButton()
+        Me.searchTxt = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.dgUserTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.addBtn)
+        Me.Panel1.Controls.Add(Me.dgUserTable)
+        Me.Panel1.Controls.Add(Me.searchButton)
+        Me.Panel1.Controls.Add(Me.searchTxt)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Location = New System.Drawing.Point(-22, 59)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1011, 673)
+        Me.Panel1.TabIndex = 19
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(117, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(688, 265)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(257, 65)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Delete"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(117, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(688, 194)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(257, 65)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "Update"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'addBtn
+        '
+        Me.addBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(117, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.addBtn.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.addBtn.Location = New System.Drawing.Point(688, 123)
+        Me.addBtn.Name = "addBtn"
+        Me.addBtn.Size = New System.Drawing.Size(257, 65)
+        Me.addBtn.TabIndex = 21
+        Me.addBtn.Text = "Add"
+        Me.addBtn.UseVisualStyleBackColor = False
+        '
+        'dgUserTable
+        '
+        Me.dgUserTable.BackgroundColor = System.Drawing.Color.White
+        Me.dgUserTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgUserTable.Location = New System.Drawing.Point(102, 123)
+        Me.dgUserTable.Name = "dgUserTable"
+        Me.dgUserTable.RowHeadersWidth = 51
+        Me.dgUserTable.RowTemplate.Height = 29
+        Me.dgUserTable.Size = New System.Drawing.Size(536, 463)
+        Me.dgUserTable.TabIndex = 20
+        '
+        'searchButton
+        '
+        Me.searchButton.FlatAppearance.BorderSize = 0
+        Me.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.searchButton.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.searchButton.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass
+        Me.searchButton.IconColor = System.Drawing.Color.Black
+        Me.searchButton.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.searchButton.IconSize = 25
+        Me.searchButton.Location = New System.Drawing.Point(343, 79)
+        Me.searchButton.Name = "searchButton"
+        Me.searchButton.Size = New System.Drawing.Size(116, 41)
+        Me.searchButton.TabIndex = 19
+        Me.searchButton.Text = "Search"
+        Me.searchButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.searchButton.UseVisualStyleBackColor = True
+        '
+        'searchTxt
+        '
+        Me.searchTxt.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.searchTxt.Location = New System.Drawing.Point(102, 83)
+        Me.searchTxt.Name = "searchTxt"
+        Me.searchTxt.Size = New System.Drawing.Size(235, 34)
+        Me.searchTxt.TabIndex = 18
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.Control
+        Me.Label5.Font = New System.Drawing.Font("Arial", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(197, Byte), Integer), CType(CType(95, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(422, 24)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(146, 24)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "User Accounts"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.Control
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(197, Byte), Integer), CType(CType(95, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(43, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 24)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Users"
+        '
+        'FormUsers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(993, 651)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FormUsers"
+        Me.Text = "FormUsers"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.dgUserTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents searchButton As FontAwesome.Sharp.IconButton
+    Friend WithEvents searchTxt As TextBox
+    Friend WithEvents dgUserTable As DataGridView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents addBtn As Button
+End Class
