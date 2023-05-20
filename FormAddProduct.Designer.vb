9015370f4e34f6f23c97e76c811fba1d68a072e7
JohnOtilla09 <otillajohn09@gmail.com>
2023-05-20 11:55:52 +0800

    Crud in Inventory

--- FormAddProduct.Designer.vb
+++ FormAddProduct.Designer.vb
@@ -22,172 +22,171 @@ Partial Class FormAddProduct
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.prodTxt = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.quantityTxt = New System.Windows.Forms.TextBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.addBtn = New System.Windows.Forms.Button()
-        Me.cancelBtn = New System.Windows.Forms.Button()
-        Me.typeTxt = New System.Windows.Forms.TextBox()
-        Me.IconPictureBox1 = New FontAwesome.Sharp.IconPictureBox()
-        CType(Me.IconPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.White
-        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
-        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(197, Byte), Integer), CType(CType(95, Byte), Integer), CType(CType(252, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(442, 50)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(129, 24)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Add Product"
-        '
-        'prodTxt
-        '
-        Me.prodTxt.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.prodTxt.Location = New System.Drawing.Point(554, 117)
-        Me.prodTxt.Name = "prodTxt"
-        Me.prodTxt.PlaceholderText = "Enter product"
-        Me.prodTxt.Size = New System.Drawing.Size(231, 30)
-        Me.prodTxt.TabIndex = 1
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(442, 121)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(78, 23)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Product"
-        '
-        'Label3
-        '
-        Me.Label3.AccessibleName = ""
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(442, 183)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(53, 23)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Type"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(442, 250)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(83, 23)
-        Me.Label4.TabIndex = 6
-        Me.Label4.Text = "Quantity"
-        '
-        'quantityTxt
-        '
-        Me.quantityTxt.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.quantityTxt.Location = New System.Drawing.Point(554, 246)
-        Me.quantityTxt.Name = "quantityTxt"
-        Me.quantityTxt.PlaceholderText = "Per sack"
-        Me.quantityTxt.Size = New System.Drawing.Size(231, 30)
-        Me.quantityTxt.TabIndex = 5
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label5.Location = New System.Drawing.Point(442, 313)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(55, 23)
-        Me.Label5.TabIndex = 8
-        Me.Label5.Text = "Price"
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox4.Location = New System.Drawing.Point(554, 313)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(231, 30)
-        Me.TextBox4.TabIndex = 7
-        '
-        'addBtn
-        '
-        Me.addBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(117, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(218, Byte), Integer))
-        Me.addBtn.ForeColor = System.Drawing.Color.Black
-        Me.addBtn.Location = New System.Drawing.Point(442, 381)
-        Me.addBtn.Name = "addBtn"
-        Me.addBtn.Size = New System.Drawing.Size(116, 41)
-        Me.addBtn.TabIndex = 9
-        Me.addBtn.Text = "Add"
-        Me.addBtn.UseVisualStyleBackColor = False
-        '
-        'cancelBtn
-        '
-        Me.cancelBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(117, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(218, Byte), Integer))
-        Me.cancelBtn.ForeColor = System.Drawing.Color.Black
-        Me.cancelBtn.Location = New System.Drawing.Point(669, 381)
-        Me.cancelBtn.Name = "cancelBtn"
-        Me.cancelBtn.Size = New System.Drawing.Size(116, 41)
-        Me.cancelBtn.TabIndex = 10
-        Me.cancelBtn.Text = "Cancel"
-        Me.cancelBtn.UseVisualStyleBackColor = False
-        '
-        'typeTxt
-        '
-        Me.typeTxt.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.typeTxt.Location = New System.Drawing.Point(554, 179)
-        Me.typeTxt.Name = "typeTxt"
-        Me.typeTxt.PlaceholderText = "Pig, Cat, Dog, Chicken"
-        Me.typeTxt.Size = New System.Drawing.Size(231, 30)
-        Me.typeTxt.TabIndex = 3
-        '
-        'IconPictureBox1
-        '
-        Me.IconPictureBox1.BackColor = System.Drawing.Color.White
-        Me.IconPictureBox1.BackgroundImage = Global.Otilla_Inventory.My.Resources.Resources.undraw_add_information_j2wg
-        Me.IconPictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.IconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.IconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.None
-        Me.IconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText
-        Me.IconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
-        Me.IconPictureBox1.IconSize = 385
-        Me.IconPictureBox1.Location = New System.Drawing.Point(12, 39)
-        Me.IconPictureBox1.Name = "IconPictureBox1"
-        Me.IconPictureBox1.Size = New System.Drawing.Size(385, 433)
-        Me.IconPictureBox1.TabIndex = 12
-        Me.IconPictureBox1.TabStop = False
-        '
-        'FormAddProduct
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(826, 512)
-        Me.Controls.Add(Me.IconPictureBox1)
-        Me.Controls.Add(Me.cancelBtn)
-        Me.Controls.Add(Me.addBtn)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.quantityTxt)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.typeTxt)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.prodTxt)
-        Me.Controls.Add(Me.Label1)
-        Me.MaximizeBox = False
-        Me.Name = "FormAddProduct"
-        Me.Text = "FormAddProduct"
-        CType(Me.IconPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        Label1 = New Label()
+        prodTxt = New TextBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        quantityTxt = New TextBox()
+        Label5 = New Label()
+        priceTxt = New TextBox()
+        addBtn = New Button()
+        cancelBtn = New Button()
+        IconPictureBox1 = New FontAwesome.Sharp.IconPictureBox()
+        typeTxt = New ComboBox()
+        CType(IconPictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.White
+        Label1.Font = New Font("Arial", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label1.ForeColor = Color.FromArgb(CByte(197), CByte(95), CByte(252))
+        Label1.Location = New Point(442, 50)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(129, 24)
+        Label1.TabIndex = 0
+        Label1.Text = "Add Product"
+        ' 
+        ' prodTxt
+        ' 
+        prodTxt.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        prodTxt.Location = New Point(554, 117)
+        prodTxt.Name = "prodTxt"
+        prodTxt.PlaceholderText = "Enter product"
+        prodTxt.Size = New Size(231, 30)
+        prodTxt.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(442, 121)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 23)
+        Label2.TabIndex = 2
+        Label2.Text = "Product"
+        ' 
+        ' Label3
+        ' 
+        Label3.AccessibleName = ""
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(442, 183)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 23)
+        Label3.TabIndex = 4
+        Label3.Text = "Type"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(442, 250)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(83, 23)
+        Label4.TabIndex = 6
+        Label4.Text = "Quantity"
+        ' 
+        ' quantityTxt
+        ' 
+        quantityTxt.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        quantityTxt.Location = New Point(544, 246)
+        quantityTxt.Name = "quantityTxt"
+        quantityTxt.PlaceholderText = "Per sack"
+        quantityTxt.Size = New Size(241, 30)
+        quantityTxt.TabIndex = 5
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(442, 313)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(55, 23)
+        Label5.TabIndex = 8
+        Label5.Text = "Price"
+        ' 
+        ' priceTxt
+        ' 
+        priceTxt.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        priceTxt.Location = New Point(554, 313)
+        priceTxt.Name = "priceTxt"
+        priceTxt.Size = New Size(231, 30)
+        priceTxt.TabIndex = 7
+        ' 
+        ' addBtn
+        ' 
+        addBtn.BackColor = Color.FromArgb(CByte(117), CByte(230), CByte(218))
+        addBtn.ForeColor = Color.Black
+        addBtn.Location = New Point(442, 381)
+        addBtn.Name = "addBtn"
+        addBtn.Size = New Size(116, 41)
+        addBtn.TabIndex = 9
+        addBtn.Text = "Add"
+        addBtn.UseVisualStyleBackColor = False
+        ' 
+        ' cancelBtn
+        ' 
+        cancelBtn.BackColor = Color.FromArgb(CByte(117), CByte(230), CByte(218))
+        cancelBtn.ForeColor = Color.Black
+        cancelBtn.Location = New Point(669, 381)
+        cancelBtn.Name = "cancelBtn"
+        cancelBtn.Size = New Size(116, 41)
+        cancelBtn.TabIndex = 10
+        cancelBtn.Text = "Cancel"
+        cancelBtn.UseVisualStyleBackColor = False
+        ' 
+        ' IconPictureBox1
+        ' 
+        IconPictureBox1.BackColor = Color.White
+        IconPictureBox1.BackgroundImage = My.Resources.Resources.undraw_add_information_j2wg
+        IconPictureBox1.BackgroundImageLayout = ImageLayout.Stretch
+        IconPictureBox1.ForeColor = SystemColors.ControlText
+        IconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.None
+        IconPictureBox1.IconColor = SystemColors.ControlText
+        IconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto
+        IconPictureBox1.IconSize = 385
+        IconPictureBox1.Location = New Point(12, 39)
+        IconPictureBox1.Name = "IconPictureBox1"
+        IconPictureBox1.Size = New Size(385, 433)
+        IconPictureBox1.TabIndex = 12
+        IconPictureBox1.TabStop = False
+        ' 
+        ' typeTxt
+        ' 
+        typeTxt.DisplayMember = "jfb"
+        typeTxt.FormattingEnabled = True
+        typeTxt.Location = New Point(554, 183)
+        typeTxt.Name = "typeTxt"
+        typeTxt.Size = New Size(231, 28)
+        typeTxt.TabIndex = 13
+        ' 
+        ' FormAddProduct
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(826, 512)
+        Controls.Add(typeTxt)
+        Controls.Add(IconPictureBox1)
+        Controls.Add(cancelBtn)
+        Controls.Add(addBtn)
+        Controls.Add(Label5)
+        Controls.Add(priceTxt)
+        Controls.Add(Label4)
+        Controls.Add(quantityTxt)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(prodTxt)
+        Controls.Add(Label1)
+        MaximizeBox = False
+        Name = "FormAddProduct"
+        Text = "FormAddProduct"
+        CType(IconPictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
@@ -197,9 +196,9 @@ Partial Class FormAddProduct
     Friend WithEvents Label4 As Label
     Friend WithEvents quantityTxt As TextBox
     Friend WithEvents Label5 As Label
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents priceTxt As TextBox
     Friend WithEvents addBtn As Button
     Friend WithEvents cancelBtn As Button
-    Friend WithEvents typeTxt As TextBox
     Friend WithEvents IconPictureBox1 As FontAwesome.Sharp.IconPictureBox
+    Friend WithEvents typeTxt As ComboBox
 End Class
--- FormAddProduct.vb
+++ FormAddProduct.vb
@@ -1,10 +1,41 @@
-﻿Public Class FormAddProduct
+﻿Imports System.Windows.Controls
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+
+Public Class FormAddProduct
     Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
-        Me.Hide()
-        MessageBox.Show("Product added successfully")
+        Dim id = 0
+
+        If typeTxt == 
+
+        With Me
+
+            Call Connect_to_DB()
+            Dim mycmd As New MySqlCommand
+
+            Try
+                strSQL = "Insert into"
+            Catch ex As Exception
+
+            End Try
+
+        End With
+
+        'Me.Hide()
+        'MessageBox.Show("Product added successfully")
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cancelBtn.Click
         Me.Hide()
+    End Sub
+
+    Private Sub FormAddProduct_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        typeTxt.DropDownStyle = ComboBoxStyle.DropDownList
+        typeTxt.Items.Add("Chicken")
+        typeTxt.SelectedIndex = 0
+        typeTxt.Items.Add("Pig")
+        typeTxt.Items.Add("Dog")
+        typeTxt.Items.Add("Cat")
+
     End Sub
 End Class
